d3c5c54b2bde82f3d7bb8ddbcf465ba0d882261e
Valen <92762944+Valen7440@users.noreply.github.com>
2024-07-02 23:05:30 -0300

    Update database.js

--- handlers/database.js
+++ handlers/database.js
@@ -5,6 +5,5 @@ const data = new Keyv(`sqlite://${path.resolve("./data/data.sqlite")}`);
 const config = new Keyv(`sqlite://${path.resolve("./data/config.sqlite")}`);
 const player = new Keyv(`sqlite://${path.resolve("./data/player.sqlite")}`);
 const metrics = new Keyv(`sqlite://${path.resolve("./data/metrics.sqlite")}`);
-// prendo el bot' sip
 
 module.exports = { data, config, metrics, player };

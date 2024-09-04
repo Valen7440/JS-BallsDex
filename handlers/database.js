b1f06f957a81f7686378318e76524a45d21b1839
Valen <92762944+Valen7440@users.noreply.github.com>
2024-09-04 15:29:39 -0300

    fix bug 'Keyv is not a constructor'

--- handlers/database.js
+++ handlers/database.js
@@ -1,9 +1,15 @@
-const Keyv = require("keyv");
+const { Keyv } = require("keyv");
+const { KeyvSqlite } = require("@keyv/sqlite")
 const path = require("node:path");
 
-const data = new Keyv(`sqlite://${path.resolve("./data/data.sqlite")}`);
-const config = new Keyv(`sqlite://${path.resolve("./data/config.sqlite")}`);
-const player = new Keyv(`sqlite://${path.resolve("./data/player.sqlite")}`);
-const metrics = new Keyv(`sqlite://${path.resolve("./data/metrics.sqlite")}`);
+const data_sqlite = new KeyvSqlite(`sqlite://${path.resolve("./data/data.sqlite")}`);
+const config_sqlite = new KeyvSqlite(`sqlite://${path.resolve("./data/config.sqlite")}`);
+const player_sqlite = new KeyvSqlite(`sqlite://${path.resolve("./data/player.sqlite")}`);
+const metrics_sqlite = new KeyvSqlite(`sqlite://${path.resolve("./data/metrics.sqlite")}`);
+
+const data = new Keyv({ store: data_sqlite });
+const config = new Keyv({ store: config_sqlite });
+const player = new Keyv({ store: player_sqlite });
+const metrics = new Keyv({ store: metrics_sqlite });
 
 module.exports = { data, config, metrics, player };
